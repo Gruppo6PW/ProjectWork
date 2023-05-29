0f8772ef464f7f275488ebe0e81ca2b44faff122
Gruppo6PW <gruppo6projectworkits@gmail.com>
2023-05-29 10:55:14 +0200

    modifica pagina login.php

--- php/login.php
+++ php/login.php
@@ -1,47 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+
+echo "Benvenuto";
+
+?>
+
+
+<html>
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <script src="js/scripts.js"></script>
     <title>Login</title>
+
+    <!-- Bootstrap CDN !-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
 
-    <h1>Login</h1>
-
     <form name="loginForm" action="index.php" method="post">
-
-        <div class="container">
-            <label for="email">E-Mail</label>
-            <br>
-            <input type="text" placeholder="inserisci e-mail" id="email" name="email" required>
-            <br>
-            <br>
-            <label for="psw">Password</label>
-            <br>
-            <input type="password" placeholder="inserisci password" id="psw" name="psw" required>
-            <br>
-            <br>
-            <button type="submit">Login</button>
-            <br>
-            <label for="ricordarmi">Ricordami</label>
-            <input type="checkbox" id="ricordami" name="ricordami">
-            <br>
-            <span class="psw">Hai dimenticato la <a href="php/passwordDimenticata.php">Password</a>?</span>
+        <div class="box">
+            <h2>Benvenuto</h2>
+            <form>
+                <br>
+                <label>E-Mail</label>
+                <div class="inputBox">
+                    <input type="email" name="email" required>
+                </div>
+                <label>Password</label>
+                <div class="inputBox">
+                    <input type="psw" name="psw" required>
+                </div>
+                <br>
+                <input type="submit" name="login" value="Login">
+            </form>
         </div>
-
     </form>
 
-    <?php
-
-
-
-    ?>
 </body>
 
 </html>
