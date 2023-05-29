35b2b8e6350976217f32e6bdbb96f456288cdd22
Gruppo6PW <gruppo6projectworkits@gmail.com>
2023-05-29 09:56:06 +0200

    organizzazione dei files in cartelle

--- php/login.php
+++ php/login.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/scripts.js"></script>
-    <title>home</title>
+    <title>Login</title>
 </head>
 
 <body>
@@ -36,6 +36,12 @@
         </div>
 
     </form>
+
+    <?php
+
+
+
+    ?>
 </body>
 
 </html>
