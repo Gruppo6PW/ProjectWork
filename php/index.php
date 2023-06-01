47db58abc92e337a3317df75aa0377ff9cf3f5d2
= <=>
2023-06-01 16:37:10 +0200

    aggiunta sessione con assegnazione ccid

--- php/index.php
+++ php/index.php
@@ -1,50 +1,54 @@
 <?php
-// Connessione database
-//$conn=new mysqli("localhost", "gruppo6", "ZQ5Z4Dzc6Ddd", "my_gruppo6");
-$conn=new mysqli("localhost", "root", "", "my_gruppo6");
-// Verifica della connessione
-if ($conn->connect_error) {
-    die("Connessione al database fallita: " . $conn->connect_error);
-}
+  // Avvio la sessione
+  session_start();
 
-//Da sistemare con session utente
-$contoCorrenteID = 1;
+  $_SESSION["accessoEseguito"] = "true";
+  $_SESSION["contoCorrenteID"] = $_GET["contoCorrenteID"];
+  $contoCorrenteID = $_GET["contoCorrenteID"];
 
-// Prepared statement per ricavare i dati utente (nome, data apertura conto)
-try{
-    $query1 = $conn->prepare("SELECT NomeTitolare, DataApertura FROM tconticorrenti WHERE ContoCorrenteID = ?");
-    $query1->bind_param("i", $contoCorrenteID);
-    $query1->execute();
-    $risultato1 = $query1->get_result();
+  // Connessione database
+  //$conn=new mysqli("localhost", "gruppo6", "ZQ5Z4Dzc6Ddd", "my_gruppo6");
+  $conn=new mysqli("localhost", "root", "", "my_gruppo6");
+  // Verifica della connessione
+  if ($conn->connect_error) {
+      die("Connessione al database fallita: " . $conn->connect_error);
+  }
 
-    $datiUtente = $risultato1->fetch_assoc();
-    $nomeUtente = $datiUtente['NomeTitolare'];
-    $dataApertura = $datiUtente['DataApertura'];
-    $query1->close();
-} catch(Exception $e){
-    echo "Qualcosa è andato storto nella richiesta dei dati dell'utente al db.";
-}
+  // Prepared statement per ricavare i dati utente (nome, data apertura conto)
+  try{
+      $query1 = $conn->prepare("SELECT NomeTitolare, DataApertura FROM tconticorrenti WHERE ContoCorrenteID = ?");
+      $query1->bind_param("i", $contoCorrenteID);
+      $query1->execute();
+      $risultato1 = $query1->get_result();
 
-// Prepared statement per ricavare le ultime 5 operazioni
-try{
-    $query2 = $conn->prepare("SELECT movimenti.MovimentoID, movimenti.Data, movimenti.Importo, movimenti.Saldo, categorie.NomeCategoria 
-    FROM tmovimenticontocorrente AS movimenti JOIN tcategoriemovimenti AS categorie ON 
-    movimenti.CategoriaMovimentoID = categorie.CategoriaMovimentoID WHERE movimenti.ContoCorrenteID = ? ORDER BY movimenti.Data 
-    DESC LIMIT 5");
-    $query2->bind_param("i", $contoCorrenteID);
-    $query2->execute();
-    $risultato2 = $query2->get_result();
-    $ultimeOperazioni = array();
-    while($row = $risultato2->fetch_assoc()){
-        $ultimeOperazioni[] = $row;
-    }
-$saldo = $ultimeOperazioni[0]['Saldo'];
-$query2->close();
-} catch(Exception $e){
-    echo "Qualcosa è andato storto nella richiesta delle operazioni al db.";
-}
+      $datiUtente = $risultato1->fetch_assoc();
+      $nomeUtente = $datiUtente['NomeTitolare'];
+      $dataApertura = $datiUtente['DataApertura'];
+      $query1->close();
+  } catch(Exception $e){
+      echo "Qualcosa è andato storto nella richiesta dei dati dell'utente al db.";
+  }
 
-$conn->close();
+  // Prepared statement per ricavare le ultime 5 operazioni
+  try{
+      $query2 = $conn->prepare("SELECT movimenti.MovimentoID, movimenti.Data, movimenti.Importo, movimenti.Saldo, categorie.NomeCategoria 
+      FROM tmovimenticontocorrente AS movimenti JOIN tcategoriemovimenti AS categorie ON 
+      movimenti.CategoriaMovimentoID = categorie.CategoriaMovimentoID WHERE movimenti.ContoCorrenteID = ? ORDER BY movimenti.Data 
+      DESC LIMIT 5");
+      $query2->bind_param("i", $contoCorrenteID);
+      $query2->execute();
+      $risultato2 = $query2->get_result();
+      $ultimeOperazioni = array();
+      while($row = $risultato2->fetch_assoc()){
+          $ultimeOperazioni[] = $row;
+      }
+  $saldo = $ultimeOperazioni[0]['Saldo'];
+  $query2->close();
+  } catch(Exception $e){
+      echo "Qualcosa è andato storto nella richiesta delle operazioni al db.";
+  }
+
+  $conn->close();
 ?>
 
 
