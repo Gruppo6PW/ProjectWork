e158fcd0450632797033192b07917304c89651f0
Dumitru Trofosila <dumitrutrofosila@gmail.com>
2023-06-01 11:21:08 +0100

    NON FUNZIONA ricercaMovimenti3.php

--- php/ricercaMovimenti3.php
+++ php/ricercaMovimenti3.php
@@ -2,8 +2,13 @@
     //!!!DA FIXARE CON LA SESSION UTENTE!!!
     $contoCorrenteID = 1;
     // controllo che vengano passati dei valori in post
-    $data_inizio = isset($_POST['data_inizio']) ? $_POST['data_inizio'] : '' ;
-    $data_fine = isset($_POST['data_fine']) ? $_POST['data_fine'] : '' ;
+    $data_periodo = isset($_POST['datefilter']) ? $_POST['datefilter'] : '';
+    if(isset($_POST['datefilter'])){
+        $date = explode(' - ', $data_periodo);
+        $data_inizio = $date[0];
+        $data_fine = $date[1];
+    }
+
 
     // Connessione database
     //$conn=new mysqli("localhost", "gruppo6", "ZQ5Z4Dzc6Ddd", "my_gruppo6");
@@ -37,12 +42,10 @@
 <html>
     <head>
         <title>Ricerca periodo</title>
-        <link rel="stylesheet" href="/css/stylesIndex.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker.min.css">
+        
     </head>
     <body>
         <header>
@@ -50,33 +53,19 @@
             <img src="Media/searchIcon.png" alt="Icona Ricerca" width=200>
             <a href="profiloUtente.php"> <img src="Media/profileIcon.png" alt="Icona Profilo Utente" width=200> </a>
             <img src="Media/transactionIcon.png" alt="Icona Operazioni" width=200>
-            </div>
-        </header>
-
+        </div>
+    </header>
+    
     <!-- Selezione del periodo attraverso datepicker (BOOTSTRAP) -->
     <h1>Seleziona un periodo:</h1>
     <form action="ricercaMovimenti3.php" method="POST">
         <div class="form-group">
-            <label for="data_inizio">Data di inizio:</label>
-            <input type="text" class="form-control datepicker" id="data_inizio" name="data_inizio">
-        </div>
-
-        <div class="form-group">
-            <label for="data_fine">Data di fine:</label>
-            <input type="text" class="form-control datepicker" id="data_fine" name="data_fine">
+            <label for="datefilter">Seleziona un periodo:</label>
+            <input type="text" name="datefilter" id="datefilter" value=""/>
         </div>
 
         <button type="submit" class="btn btn-primary">Invia</button>
     </form>
-
-    <script>
-        $(document).ready(function() {
-            $('.datepicker').datepicker({
-                format: 'yyyy-mm-dd',
-                autoclose: true
-            });
-        });
-    </script>
 
         <?php if ($operazioni != null): ?> <!-- Verifica se è stata selezionata una categoria valida -->
         <h2>Storico operazioni:</h2>
@@ -105,6 +94,30 @@
     </table>
     <?php endif ?>
 
+            <script>
+        $(document).ready(function() {
+            $('input[name="datefilter"]').daterangepicker({
+                autoUpdateInput: false,
+                locale: {
+                    cancelLabel: 'Clear'
+                }
+            });
+    
+            $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+                $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+            });
+    
+            $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+                $(this).val('');
+            });
+        });
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+   
     </body>
 </html>
 
