9f7eab4033b725e5c8c19026888d1ca700dcf4c8
= <=>
2023-06-03 12:00:35 +0200

    aggiunto controllo sessione in base alla data

--- php/index.php
+++ php/index.php
@@ -2,19 +2,19 @@
   // Avvio la sessione
   session_start();
 
+  // Prendo l'id del conto corrente nell'URL
+  $contoCorrenteID = $_GET["contoCorrenteID"];
+
+  // Connessione database
+  $conn=new mysqli("localhost", "gruppo6", "ZQ5Z4Dzc6Ddd", "my_gruppo6");
+  //$conn=new mysqli("localhost", "root", "", "my_gruppo6");
+
+  // Verifica della connessione
+  if ($conn->connect_error) {
+    die("Connessione al database fallita: " . $conn->connect_error);
+  }
+
   if($_SESSION["accessoEseguito"]){
-    $_SESSION["accessoEseguito"] = "true";
-    $_SESSION["contoCorrenteID"] = $_GET["contoCorrenteID"];
-    $contoCorrenteID = $_GET["contoCorrenteID"];
-  
-    // Connessione database
-    $conn=new mysqli("localhost", "gruppo6", "ZQ5Z4Dzc6Ddd", "my_gruppo6");
-    //$conn=new mysqli("localhost", "root", "", "my_gruppo6");
-    // Verifica della connessione
-    if ($conn->connect_error) {
-        die("Connessione al database fallita: " . $conn->connect_error);
-    }
-  
     // Prepared statement per ricavare i dati utente (nome, data apertura conto)
     try{
         $query1 = $conn->prepare("SELECT NomeTitolare, DataApertura FROM tconticorrenti WHERE ContoCorrenteID = ?");
@@ -48,14 +48,67 @@
       $query2->close();
       } catch(Exception $e){
           echo "Qualcosa è andato storto nella richiesta delle operazioni al db.";
-      }
-    
-      $conn->close();    
+      }  
   } else{
-    // Non ha l'accesso, lo reinderizzo al login
-    header("Location: http://gruppo6.altervista.org/ProjectWork/php/login.php");
-  }
+      // Controllo nel db se l'accesso valido è true e la data dell'ultimo accesso. In quel caso gli creo la sessione, altrimenti lo mando al login
+      $SQL = "SELECT AccessoValido, Data FROM taccessi WHERE ContoCorrenteID = ? ORDER BY Data DESC LIMIT 1";
+      if ($statement = $conn->prepare($SQL)) {
+        $statement->bind_param("i", $contoCorrenteID);
+        $statement->execute();
 
+        // Prendo il risultato della query
+        $result = $statement->get_result();
+
+        // Imposto inizialmente l'AccessoValido a 0. Se poi l'utente si è effettivamente registrato allora lo imposto a 1
+        $accessoValido = 0;
+
+        // C'è una tupla, prendo il NumeroTentativiLogin
+        if ($result->num_rows != 0) {
+            // Salvo il contenuto del result
+            while ($row = $result->fetch_assoc()) {
+                // Prendo l'AccessoValido
+                $accessoValido = $row["AccessoValido"];
+                $dataUltimoAccesso = $row["Data"];
+            }
+        }
+
+        // Controllo se è variato il valore di accessoValido
+        if($accessoValido == 1){
+          $dataCorrenteString = date("Y-m-d") . " " . date("h:i:s");
+          $dataCorrente = date_create($dataCorrenteString);
+
+          // Converto la data letta dal db in oggetto Date di php
+          $dataDB = date_create($dataUltimoAccesso);
+
+          // Calcolo la differenza di tempo
+          $differenza = date_diff($dataCorrente, $dataDB);
+
+          // Controllo se son passate meno di 24 dall'ultimo login valido
+          if($differenza -> days == 0 && $differenza -> h < 24){
+            // Accesso ancora valido, creo la sessione
+            $_SESSION["accessoEseguito"] = "true";
+            $_SESSION["contoCorrenteID"] = $contoCorrenteID;
+          } else{
+            // Troppo tempo dall'ultimo accesso. Lo mando al login
+            header("Location: https://gruppo6.altervista.org/ProjectWork/php/login.php");
+          }
+
+        } else{
+          // Chiudo la connessione al db
+          $conn->close();  
+  
+          // Non ha l'accesso, lo reinderizzo al login
+          header("Location: https://gruppo6.altervista.org/ProjectWork/php/login.php");
+        }
+      } else {
+          // C'è stato un errore, lo stampo
+          $errore = $mysqli->errno . ' ' . $mysqli->error;
+          echo $errore;
+      }
+
+    // Chiudo la connessione al db
+    $conn->close();  
+  }
 ?>
 
 
