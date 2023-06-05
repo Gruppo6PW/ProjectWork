e398ebdfdf798919576031abc1333057762a2c87
= <=>
2023-06-05 12:18:45 +0200

    html giusto + sistemati errori

--- php/passwordDimenticata.php
+++ php/passwordDimenticata.php
@@ -6,7 +6,6 @@
         if (preg_match($emailRegex, $stringaDaControllare) == 1) {
             return true;
         } else{
-            echo ("<a href='http://gruppo6.altervista.org/ProjectWork/passwordDimenticata.php'>Torna alla pagina per la password dimenticata</a>");
             return false;
         }
     }
@@ -54,28 +53,68 @@
             $specificheHtml = "MIME-Version: 1.0" . "\r\n" . "Content-type:text/html;charset=UTF-8" . "\r\n";
             mail("$email", "Reimposta Password - Project Work", $msg, $specificheHtml);
 
-            // Creo una variabile per modificare l'html visualizzato
+            // Form nel caso in cui sia inviata con successo la mail
             $html = "
-            <h2>Ti è stata inviata una email contenente un link per reimpostare la password</h2>
+            <div class='registration-form'>
+            	<form action='' name='formPasswordDimenticata' method='POST' >
+                	<div class='form-icon'>
+               			<!-- Codice per l'icona SVG  -->
+                        <span>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' fill='#dee9ff' class='bi bi-envelope-fill' viewBox='0 0 16 12'>
+                                <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
+                            </svg>
+                        </span>
+                	</div>
+                    <div class='form-group'>
+                        <h3>Ti è stata inviata una email contenente un link per reimpostare la password</h3>
+                    </div>
+                </form>        
+            </div>
             ";
 
         } else {
-            $html = "<h2>L'email non è valida</h2>";
-            return;
+            $html = "
+            <div class='registration-form'>
+                <form action='' name='formPasswordDimenticata' method='POST' >
+                    <div class='form-icon'>
+                        <!-- Codice per l'icona SVG  -->
+                        <span>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' fill='#dee9ff' class='bi bi-envelope-fill' viewBox='0 0 16 12'>
+                                <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
+                            </svg>
+                        </span>
+                    </div>
+                    <div class='form-group'>
+                        <h3>L'email non è valida</h3>
+                    </div>
+                </form>        
+            </div>
+            ";
         }
     } else{
-        // Carica html della form
+        // Form in cui l'utente inserisce la mail
         $html = "
-        <h1>Inserisci la tua mail, in modo da ricevere il link per reimpostare la password</h1>
-
-        <form action='' name='formPasswordDimenticata' method='POST'>
-            <label for='email'>Email:</label>
-            <input type='email' name='email' id='emailID' required>
-    
-            <br>
-    
-            <input type='submit' value='Invia' name='Invia'>
-        </form>
+        <div class='registration-form'>
+            <form action='' name='formPasswordDimenticata' method='POST' >
+                <div class='form-icon'>
+                    <!-- Codice per l'icona SVG  -->
+                    <span>
+                        <svg xmlns='http://www.w3.org/2000/svg' height='70' width='70' fill='#dee9ff' class='bi bi-key-fill' viewBox='-1 0 17 9'>
+                            <path d='M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z'/>
+                        </svg>
+                    </span>
+                </div>
+                <div class='form-group'>
+                	<p> Inserisci la tua mail, in modo da ricevere il link per reimpostare la password: </p>
+           		</div>
+                <div class='form-group'>
+                    <input type='email' class='form-control item' name='email' id='emailID' placeholder='E-mail' required>
+                </div>
+                <div class='form-group'>
+                    <input type='submit' class='btn btn-block create-account' name='Invia'>
+                </div>
+            </form>
+        </div>
         ";
     }
 ?>
@@ -87,9 +126,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Dimenticata</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styleNoSocial.css">
 </head>
 <body>
     <!-- HTML -->
     <?php echo $html; // Si adatta ad ogni caricamento ?>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <script src="assets/js/script.js"></script>
 </body>
 </html>
