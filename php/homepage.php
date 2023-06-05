e17ebda8adeb4f7635580af187d0adfcee989c0d
Dumitru Trofosila <dumitrutrofosila@gmail.com>
2023-06-05 08:07:46 +0200

    bozza homepage

---
+++ php/homepage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Benvenuto nella Banca XYZ</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="homepage.php">Home</a></li>
+        <li><a href="login.php">Area Clienti</a></li>
+        <li><a href="#">Contattaci</a></li>
+      </ul>
+    </nav>
+    <div>
+      <a href="login.php">Accedi</a>
+      <a href="registrazione.php">Registrati</a>
+    </div>
+  </header>
+
+  <main>
+      <h2>Benvenuto nella Banca XYZ</h2>
+    <h1>Il tuo viaggio verso il successo finanziario inizia con noi.</h1> 
+
+    <p>Siamo un istituto finanziario all'avanguardia che valorizza il rapporto umano in un mondo digitale. Forniamo strumenti e servizi bancari innovativi e crediamo fermamente nell'importanza di una consulenza personalizzata e di un supporto diretto. 
+  Siamo qui per accompagnarti in ogni fase del tuo percorso finanziario, mettendo a tua disposizione una squadra di esperti pronti a soddisfare le tue esigenze e ad aiutarti a raggiungere i tuoi obiettivi in modo sicuro e conveniente.</p>
+</main>
+ 
+
+  <footer>
+    <div>
+      <h3>Contattaci</h3>
+      <p>Indirizzo: Via Vigonovoese, 84</p>
+      <p>Città: Padova</p>
+      <p>Email: info@bancaxyz.com</p>
+      <p>Telefono: 800906060</p>
+    </div>
+  </footer>
+</body>
+</html>
