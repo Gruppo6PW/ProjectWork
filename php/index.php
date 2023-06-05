6106d7dfe11df411e144779001ab04ea0e91d462
= <=>
2023-06-05 12:19:33 +0200

    sistemato problema index non popolata+aggiuntoHtml

--- php/index.php
+++ php/index.php
@@ -16,29 +16,32 @@
 
   // Controllo se c'è una sessione attiva
   if(session_status() === PHP_SESSION_ACTIVE){
-    if($_SESSION["accessoEseguito"]){
+    if($_SESSION["accessoEseguito"] && $_SESSION["contoCorrenteID"] == $contoCorrenteID){
       //Ricavo i dati dell'utente (nome, data apertura conto)
       try{
         $SQL = "SELECT NomeTitolare, DataApertura FROM tconticorrenti WHERE ContoCorrenteID = ? LIMIT 1";
         if($statement = $conn -> prepare($SQL)){
-            $statement -> bind_param("i", $contoCorrenteID);
-            $statement -> execute();
-    
-            // Prendo il risultato della query
-            $result = $statement->get_result();
-    
-            // C'è una tupla
-            if ($result->num_rows != 0) {
-              // Salvo il contenuto del result
-              while ($row = $result->fetch_assoc()) {
-                  // Prendo l'AccessoValido
-                  $nomeUtente = $datiUtente["NomeTitolare"];
-                  $dataApertura = $datiUtente["DataApertura"];
-              }
+          $statement -> bind_param("i", $contoCorrenteID);
+          $statement -> execute();
+          
+          // Prendo il risultato della query
+          $result = $statement->get_result();
+
+          // C'è una tupla
+          if ($result->num_rows != 0) {
+            // Salvo il contenuto del result
+            while ($row = $result->fetch_assoc()) {
+              // Prendo l'AccessoValido
+              $nomeUtente = $row["NomeTitolare"];
+              $dataAperturaDB = $row["DataApertura"];
+
+              // Converto nel formato che mi serve
+              $dataApertura = date("d/m/Y", strtotime($dataAperturaDB));
             }
-    
-            // Chiudo lo statement
-            $statement->close();
+          }
+  
+          // Chiudo lo statement
+          $statement->close();
         } else{
             // C'è stato un errore, lo stampo
             $errore = $mysqli->errno . ' ' . $mysqli->error;
@@ -147,7 +150,7 @@
         }
     }
   }
-  
+
   // Chiudo la connessione al db
   $conn->close();  
 ?>
@@ -155,91 +158,90 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <style>
-    body {
-      background-color: #f8f9fa;
-    }
-    #centrata{
-      text-align:center;
-    }
-  </style>
-</head>
-<body>
-  
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary ">
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav ">
-          <li class="nav-item active">
-              <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/index.php">Home</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/profilo.php">Profilo <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item dropdown active ">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLink" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Operazioni</a>
-            <div class="dropdown-menu rounded bg-light"  aria-labelledby="navbarDropdownLink">
-              <a class="dropdown-item " href="LINK ALLA PAGINA DEL BONIFICO">Bonifico</a>
-              <a class="dropdown-item" href="LINK ALLA PAGINA DELLA RICARICA TELEFONICA">Ricarica telefonica</a>
-              <!-- <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a> -->
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+      body {
+        background-color: #f8f9fa;
+      }
+      #centrata{
+        text-align:center;
+      }
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary ">
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ">
+              <li class="nav-item active">
+                  <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/index.php">Home</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/profilo.php">Profilo <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item dropdown active ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLink" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Operazioni</a>
+                <div class="dropdown-menu rounded bg-light"  aria-labelledby="navbarDropdownLink">
+                  <a class="dropdown-item " href="LINK ALLA PAGINA DEL BONIFICO">Bonifico</a>
+                  <a class="dropdown-item" href="LINK ALLA PAGINA DELLA RICARICA TELEFONICA">Ricarica telefonica</a>
+                  <!-- <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a> -->
+                </div>
+              </li>
+              <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownDisabled" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Movimenti</a>
+                <div class="dropdown-menu rounded bg-light " aria-labelledby="navbarDropdownDisabled">
+                  <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti1.php">Ultimi movimenti</a>
+                  <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti2.php">Cerca per categoria</a>
+                  <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti3.php">Cerca per data</a>
+                </div>
+              </li>
+            </ul>
+
+            <!-- Logout -->
+            <div class="nav-item dropdown bg-danger ml-auto ">
+                <a class="nav-link active" href="http://gruppo6.altervista.org/ProjectWork/php/logOut.php" style="color: white ">LogOut</a>
             </div>
-          </li>
-          <li class="nav-item dropdown active">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownDisabled" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Movimenti</a>
-            <div class="dropdown-menu rounded bg-light " aria-labelledby="navbarDropdownDisabled">
-              <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti1.php">Ultimi movimenti</a>
-              <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti2.php">Cerca per categoria</a>
-              <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti3.php">Cerca per data</a>
-            </div>
-          </li>
-        </ul>
+          </div>
+        </nav>
 
-        <!-- Logout -->
-        <div class="nav-item dropdown bg-danger ml-auto ">
-            <a class="nav-link active" href="http://gruppo6.altervista.org/ProjectWork/php/logOut.php" style="color: white ">LogOut</a>
-        </div>
-      </div>
-    </nav>
+    <main class="container my-5">
+      <br>
+      <br>
 
-<main class="container my-5">
-  <br>
-  <br>
+      <h1 class="mb-4" id="centrata">Benvenuto, <?php echo $nomeUtente; ?>!</h1>
+      <p id="centrata">Conto creato in data: <?php echo $dataApertura; ?></p>
+      <p id="centrata">Saldo totale: <?php echo $saldo; ?></p>
 
-  <h1 class="mb-4" id="centrata">Benvenuto, <?php echo $NomeUtente; ?>!</h1>
-  <p id="centrata">Conto creato in data: <?php echo $DataApertura; ?></p>
-  <p id="centrata">Saldo totale: <?php echo $Saldo; ?></p>
-   <h4>Ultimi movimenti</h4>
-  <table class="table table-bordered mt-4 table-hover">
-    
-    <thead  >
-      <tr class="table-primary">
-        <th scope="col">Tipo operazione</th>
-        <th scope="col">Importo</th>
-        <th scope="col">Data</th>
-        <th scope="col">Dettagli</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($movimenti as $movimento): ?>
-        <tr>
-          <td><?php echo $movimento['CategoriaMovimentoID']; ?></td>
-          <td><?php echo $movimento['Importo']; ?></td>
-          <td><?php echo $movimento['Data']; ?></td>
-          <td><a href="http://gruppo6.altervista.org/ProjectWork/php/DettaglioMovimento.php?id=<?php echo $movimento['MovimentoID']; ?>" target="_blank"><img src="http://gruppo6.altervista.org/ProjectWork/css/Immagini/details.png" alt="Icona Dettagli" height="25" width="25"></a></td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-</main>
+      <h4>Ultimi movimenti</h4>
+      <table class="table table-bordered mt-4 table-hover">
+        <thead>
+          <tr class="table-primary">
+            <th scope="col">Tipo operazione</th>
+            <th scope="col">Importo</th>
+            <th scope="col">Data</th>
+            <th scope="col">Dettagli</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($ultimeOperazioni as $operazione): ?>
+            <tr>
+              <td><?php echo $operazione['CategoriaMovimentoID']; ?></td>
+              <td><?php echo $operazione['Importo']; ?></td>
+              <td><?php echo $operazione['Data']; ?></td>
+              <td><a href="http://gruppo6.altervista.org/ProjectWork/php/DettaglioMovimento.php?id=<?php echo $operazione['MovimentoID']; ?>" target="_blank"><img src="http://gruppo6.altervista.org/ProjectWork/css/Immagini/details.png" alt="Icona Dettagli" height="25" width="25"></a></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </main>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </body>
 </html>
