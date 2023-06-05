c4bedcfb20170a0600f5cd34ec31ef5fdb22a6e7
= <=>
2023-06-05 09:47:47 +0200

    indentato + css

--- php/dettaglioMovimento.php
+++ php/dettaglioMovimento.php
@@ -60,3 +60,93 @@ try{
         </tr>
     </table>
 </body>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dettagli Operazione</title>
+        <link rel="stylesheet" href="/css/stylesIndex.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <style>
+            body {
+            background-color: #f8f9fa;
+            }
+            #centrata{
+            text-align:center;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary ">
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/index.php">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="http://gruppo6.altervista.org/ProjectWork/php/profilo.php">Profilo <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown active ">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLink" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Operazioni</a>
+                        <div class="dropdown-menu rounded bg-light"  aria-labelledby="navbarDropdownLink">
+                            <a class="dropdown-item " href="http://gruppo6.altervista.org/ProjectWork/php/bonifico.php">Bonifico</a>
+                            <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricarica.php">Ricarica telefonica</a>
+                            <!-- <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a> -->
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown active">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownDisabled" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">Movimenti</a>
+                        <div class="dropdown-menu rounded bg-light " aria-labelledby="navbarDropdownDisabled">
+                            <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti1.php">Ultimi movimenti</a>
+                            <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti2.php">Cerca per categoria</a>
+                            <a class="dropdown-item" href="http://gruppo6.altervista.org/ProjectWork/php/ricercaMovimenti3.php">Cerca per data</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+            
+        <main class="container my-5">
+            <br> 
+            <br>
+
+            <h2 class="mb-4" id="centrata">Dettaglio movimento:</h2>
+
+            <table class="table table-bordered mt-4 table-hover">
+                <thead>
+                    <tr class="table-primary">
+                        <th scope="col">Tipo Operazione</th>
+                        <th scope="col"> Data</th>
+                        <th scope="col">Importo </th>
+                        <th scope="col">Descrizione</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>	
+                        <td><?php echo $datiMovimento['NomeCategoria'];?></td>
+                        <td><?php echo $datiMovimento['Data']; ?></td>
+                        <td><?php echo $datiMovimento['Importo']; ?>€</td>
+                        <td><?php echo $datiMovimento['DescrizioneEstesa']; ?></td>
+                    </tr>
+                </tbody>
+            </table>
+                        
+            <button style="font-size: 20px; background: #007bff; color:#ffffff; font-family: " type='button' class='btn btn-block create-account ' name='Indietro' onclick='functionIndietro()'>Indietro</button>
+                        
+            <script>
+                function functionIndietro()
+                {
+                            
+                }
+            </script>
+                    
+        </main>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
